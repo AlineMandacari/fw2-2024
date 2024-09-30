f6f3ef366bf2458ba7128e0792a318098b2f9abf
AlineMandacari <120032526+AlineMandacari@users.noreply.github.com>
2024-09-30 14:12:47 +0000

    Aula 30-09

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,24 @@
 import Footer from "./modulos/footer";
 import Navbar from "./modulos/navbar";
-
-export default function Index() {
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+function Index() {
   return <div>
     <Navbar />
-    Bem vindosss!!
+    Bem vindosss!! 
+    <Container>
+      <Row>
+        <Col className="bg-light text-dark">1 of 2</Col>
+        <Col className="bg-body-tertiary">2 of 2</Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
     <Footer />
     </div>;
 }
+export default Index;
