3fb92047fbcc447a08a64566654524133e493b33
AlineMandacari <120032526+AlineMandacari@users.noreply.github.com>
2024-10-14 14:21:30 +0000

    aula 14-10

--- src/pages/modulos/navbar.js
+++ src/pages/modulos/navbar.js
@@ -1,5 +1,18 @@
-function Navbar() {
-    return <div>Menu</div>
-
+import {Container, Nav, Navbar} from 'react-bootstrap';
+import styles from "@/styles/Navbar.module.css"
+export default function Menu() {
+  return (
+    <Navbar expand="sm" className={`${styles.navGradiente} bg-dark navbar-dark px-3 mx-5 my-5>`}>
+      <Container>
+        <Navbar.Brand href="/">6A</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#link">Produtos</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
-export default Navbar;
