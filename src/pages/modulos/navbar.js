c60fcb2f6075560279f3e30b2e2b95e1e3c95d3b
AlineMandacari <120032526+AlineMandacari@users.noreply.github.com>
2024-11-29 12:00:57 +0000

    aula 29-11 (2)

--- src/pages/modulos/navbar.js
+++ src/pages/modulos/navbar.js
@@ -11,12 +11,8 @@ export default function Menu() {
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/produtos">Produtos</Nav.Link>
             <Nav.Link href="/promessa">Promessa</Nav.Link>
-<<<<<<< HEAD
-            <Nav.Link href="/pages">Pages</Nav.Link> 
-=======
             <Nav.Link href="/pages">Pages</Nav.Link>
             <Nav.Link href="/pesportes">Pesportes</Nav.Link>
->>>>>>> 51671a4 (aula 14-11)
           </Nav>
         </Navbar.Collapse>
       </Navbar>
